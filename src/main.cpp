f8176d86d88e8ea758b75426dd7dbe81e70762f7
MarcoAntOliveira <marcoolivera096@gmail.com>
2025-11-24 12:16:05 -0300

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,24 @@
 #include <VL53L0X.h>
 #include <Adafruit_MPU6050.h>
 
-// Objetos dos sensores
+// Sensores
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
-//VL53L0X sensorVL;
 Adafruit_MPU6050 mpu;
 
-// Mutex do I2C
+// GPIOs
+#define LED_A     2
+#define LED_B     4
+
+// Mutex para o barramento I2C
 SemaphoreHandle_t i2cMutex;
 
-// ----------- TASK DO MAGNETÔMETRO ------------
+
+// ------------------ TASK MAGNETÔMETRO ------------------
 void taskMag(void *param){
+    pinMode(LED_A, OUTPUT);
+
     while(true){
+        digitalWrite(LED_A, HIGH);
 
         xSemaphoreTake(i2cMutex, portMAX_DELAY);
 
@@ -29,67 +36,25 @@ void taskMag(void *param){
         Serial.print(event.magnetic.y);
         Serial.print(" Z: ");
         Serial.println(event.magnetic.z);
-        
+
         xSemaphoreGive(i2cMutex);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+
+        digitalWrite(LED_A, LOW);
+
+        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
 
-// // ----------- TASK DO VL53L0X ------------
-// void taskVL53(void *param){
-//     while(true){
 
-//         xSemaphoreTake(i2cMutex, portMAX_DELAY);
 
-//         uint16_t dist = sensorVL.readRangeSingleMillimeters();
-        
-//         if(sensorVL.timeoutOccurred()){
-//             Serial.println("[TASK_VL53] Timeout!");
-//         } else {
-//             Serial.print("[TASK_VL53] ");
-//             Serial.print(dist);
-//             Serial.println(" mm");
-//         }
-
-//         xSemaphoreGive(i2cMutex);
-//         vTaskDelay(200 / portTICK_PERIOD_MS);
-//     }
-// }
-
-// ----------- TASK DO GY-521 (MPU6050) ------------
-void taskGY521 - (void *param){
-    sensors_event_t a, g, temp;
-
-    while(true){
-
-        xSemaphoreTake(i2cMutex, portMAX_DELAY);
-
-        mpu.getEvent(&a, &g, &temp);
-
-        Serial.print("[TASK_GY521] ACC X: ");
-        Serial.print(a.acceleration.x);
-        Serial.print(" Y: ");
-        Serial.print(a.acceleration.y);
-        Serial.print(" Z: ");
-        Serial.println(a.acceleration.z);
-
-        Serial.print("[TASK_GY521] GYRO X: ");
-        Serial.print(g.gyro.x);
-        Serial.print(" Y: ");
-        Serial.print(g.gyro.y);
-        Serial.print(" Z: ");
-        Serial.println(g.gyro.z);
-
-        xSemaphoreGive(i2cMutex);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
-    }
-}
-
-// ----------- TASK DO GY-521 (MPU6050) ------------
+// ------------------ TASK MPU6050 (GY-521) ------------------
 void taskGY521(void *param){
+    pinMode(LED_B, OUTPUT);
+
     sensors_event_t a, g, temp;
 
     while(true){
+        digitalWrite(LED_B, HIGH);
 
         xSemaphoreTake(i2cMutex, portMAX_DELAY);
 
@@ -110,22 +75,24 @@ void taskGY521(void *param){
         Serial.println(g.gyro.z);
 
         xSemaphoreGive(i2cMutex);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+
+        digitalWrite(LED_B, LOW);
+
+        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
 
-// ------------------- SETUP -------------------
+
+
+// ----------------------- SETUP -----------------------
 void setup(){
     Serial.begin(115200);
     Wire.begin(21,22);
 
-    // Inicializa sensores (SEM mutex, pois só ocorre no setup)
+    // Inicializa sensores
     if(!mag.begin()){
         Serial.println("Falha ao iniciar magnetometro!");
     }
-
-    sensorVL.init();
-    sensorVL.setTimeout(500);
 
     if(!mpu.begin()){
         Serial.println("Falha ao iniciar MPU6050!");
@@ -135,15 +102,17 @@ void setup(){
         mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
     }
 
-    // Cria mutex
+    // Mutex do I2C
     i2cMutex = xSemaphoreCreateMutex();
 
-    // Cria tasks
+    // Cria Tasks
     xTaskCreate(taskMag,   "MagTask",   4096, NULL, 1, NULL);
-    //xTaskCreate(taskVL53,  "VL53Task",  4096, NULL, 2, NULL);
-    xTaskCreate(taskGY521, "GY521Task", 4096, NULL, 3, NULL);
+    xTaskCreate(taskGY521, "GY521Task", 4096, NULL, 2, NULL);
 }
 
+
+
+// ------------------------ LOOP ------------------------
 void loop(){
     vTaskDelay(pdMS_TO_TICKS(1000));
 }
